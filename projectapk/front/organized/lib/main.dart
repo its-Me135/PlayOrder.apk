44b6e8154023414727ce0fc67193c1e48ad2f5f0
itsme <ggubyajoshua@gmail.com>
2025-12-18 16:12:40 +0300

    styles 1

--- projectapk/front/organized/lib/main.dart
+++ projectapk/front/organized/lib/main.dart
@@ -21,10 +21,75 @@ class _HomeState extends State<Home> {
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
+      theme: ThemeData(
+        // brightness: Brightness.light,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 20, 1, 44),
+      ),
       home: Scaffold(
-        appBar: AppBar(title: Text('Ordered Playlist ')),
+        appBar: AppBar(
+          leading: Icon(Icons.playlist_play),
+          title: Text(
+            'Ordered Playlist ',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.blueGrey,
+        ),
         body: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 25.0,
+                horizontal: 8.0,
+              ),
+              child: Container(
+                child: Center(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/logo.png',
+                            width: 100,
+                            height: 120,
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: ShaderMask(
+                              blendMode: BlendMode.srcIn,
+                              shaderCallback: (Rect bounds) {
+                                return LinearGradient(
+                                  begin: Alignment
+                                      .topRight, // Adjust based on 280deg
+                                  end: Alignment.bottomLeft,
+                                  colors: [
+                                    Color(0xFF8430F2), // #8430f2
+                                    Color(0xFF7022A1), // #7022a1
+                                    Color(0xFF24F2E8), // #24f2e8
+                                  ],
+                                  stops: [
+                                    0.0,
+                                    0.44,
+                                    1.0,
+                                  ], // Your percentages: 0%, 44%, 100%
+                                ).createShader(bounds);
+                              },
+                              child: Text(
+                                'Your Gradient Text',
+                                style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             Center(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -35,13 +100,13 @@ class _HomeState extends State<Home> {
                   color: Colors.grey[400],
                   child: Container(
                     padding: const EdgeInsets.all(16.0),
-                    height: 400,
+                    height: 300,
                     child: Column(
                       children: [
                         Align(
                           alignment: Alignment.centerLeft,
                           child: Text(
-                            'PlayList URL :',
+                            'TARGET PLAYLIST URL :',
                             style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
@@ -60,7 +125,7 @@ class _HomeState extends State<Home> {
                         Align(
                           alignment: Alignment.centerLeft,
                           child: Text(
-                            'PlayList URL :',
+                            'LOCAL DIRECTORY PATH :',
                             style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
@@ -71,25 +136,62 @@ class _HomeState extends State<Home> {
                         TextField(
                           decoration: InputDecoration(
                             border: OutlineInputBorder(),
-                            labelText: 'Enter PlayList URL',
+                            labelText: 'Downloads/videos/playlist',
                           ),
                         ),
 
                         SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Text(
+                              'DRY RUN (PREVIEW ONLY)',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                            SizedBox(width: 10),
+                            Switch(
+                              value: false,
+                              onChanged: (bool value) {
+                                // Handle switch state change
+                              },
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 20),
-            SizedBox(
-              width: 500,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Handle button press
-                },
-                child: Text('ORGANIZE & RENAME'),
+            //SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey,
+                  ),
+
+                  onPressed: () {
+                    // Handle button press
+                  },
+                  child: Text(
+                    'ORGANIZE & RENAME',
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: Image.asset(
+                'assets/images/mechanisim.png',
+                width: 200,
+                height: 100,
               ),
             ),
           ],
